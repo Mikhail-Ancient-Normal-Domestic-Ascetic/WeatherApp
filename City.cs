d36d7f7cd6b2279863dc8285bdeec0ab9058b636
student <student@B206-10>
2023-02-27 11:59:11 +0700

    Добавьте файлы проекта.

---
+++ City.cs
@@ -0,0 +1,18 @@
+﻿namespace WeatherApp
+{
+    class City
+    {
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            private set => _name = value;
+        }
+
+        public City(string name)
+        {
+            _name = name;
+        }
+    }
+}
